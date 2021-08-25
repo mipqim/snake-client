5651853ad0e0127a29b2bfce209a6940a0595c45
Sanghwan Jeon <jeff.shjeon@gmail.com>
2021-08-25 14:29:54 -0700

    Exported connect function of client.js

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require("net");
+
+const connect = function () {
+
+  console.log("Connecting ...");
+  
+  const conn = net.createConnection({
+    host: '135.23.223.133',
+    port: 50542,
+  });
+
+  conn.setEncoding("utf8");
+
+  conn.on('connect', () => {
+    console.log("Connected");
+  });
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
