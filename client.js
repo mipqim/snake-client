16b1623f0417530575e08bcca0317a002a3f5ecf
Sanghwan Jeon <jeff.shjeon@gmail.com>
2021-08-25 15:25:20 -0700

    Moving test

--- client.js
+++ client.js
@@ -14,10 +14,37 @@ const connect = function () {
   conn.on('connect', () => {
     conn.write('Name: @@@');
     console.log("Connected");    
+
+
+    for(let i = 0; i < 15; i++){
+      setTimeout(()=>{
+        conn.write('Move: up');
+        console.log("Move up");
+      }, 0+i*200);
+      setTimeout(()=>{
+        conn.write('Move: left')
+        console.log("Move left");
+      }, 500+i*200);
+      setTimeout(()=>{
+        conn.write('Move: down')
+        console.log("Move down");
+      }, 1000+i*200);      
+      setTimeout(()=>{
+        conn.write('Move: right')
+        console.log("Move right");
+      }, 1500+i*200);            
+    }
+
   });
+
+  // conn.on('data', (data) => {
+  //   console.log('Server says: ', data);
+  // });
 
   conn.on('data', (data) => {
     console.log('Server says: ', data);
+
+
   });
 
   return conn;
